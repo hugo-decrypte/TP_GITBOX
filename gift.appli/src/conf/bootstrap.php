b5f2ce0d4b52c2cda5c1b1a45e031122aa90c434
bocchiarati <88000sofien@gmail.com>
2025-05-13 19:50:11 +0200

    implémentation de twig

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -3,9 +3,15 @@
 
 use gift\appli\utils\Eloquent;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 Eloquent::init(__DIR__.'/db.ini');
+
+$loader = new FilesystemLoader(__DIR__.'/../views');
+$twig = new Environment($loader, ['debug' => true]);
+
 $app = AppFactory::create();
-$app = (require_once __DIR__ . '/routes.php')($app);
+$app = (require_once __DIR__ . '/routes.php')($app, $twig);
 
 return $app;
