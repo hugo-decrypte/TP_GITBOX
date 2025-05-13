1892e3eef100f5a321118417aa52c355cd5bc429
bocchiarati <88000sofien@gmail.com>
2025-05-13 17:03:49 +0200

    Class CategoriesAction

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use gift\appli\Controllers\CategoriesAction;
 use gift\appli\models\Categorie;
 use gift\appli\models\Prestation;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -8,20 +9,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 
 return function ($app) {
-    $app->get('/categories', function (Request $request, Response $response, array $args) {
-        $categories = Categorie::all();
-        $first = true;
-        foreach ($categories as $category) {
-            if (!$first) {
-                $response->getBody()->write(" - ");
-            } else {
-                $first = false;
-            }
-            $response->getBody()->write("<a href='/categories/{$category->id}'> {$category->libelle}</a>");
-        }
-        return $response;
-    });
-
+    $app->get('/categories', new CategoriesAction());
     $app->get('/categories/{id}', function (Request $request, Response $response, array $args) {
         $categories = Categorie::all()->where('id','=',$args['id']);
         $res = "";
