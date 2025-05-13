debdced81e2b632a10fdf0e106677d58e18a42a3
bocchiarati <88000sofien@gmail.com>
2025-05-13 19:51:05 +0200

    utilisation de twig détail catégorie

--- gift.appli/src/Controllers/CategorieIdAction.php
+++ gift.appli/src/Controllers/CategorieIdAction.php
@@ -9,18 +9,22 @@ use Slim\Psr7\Response;
 
 class CategorieIdAction extends AbstractAction {
 
+    private $twig;
+    public function __construct($twig) {
+        $this->twig = $twig;
+    }
+
     public function __invoke(Request $request, Response $response, array $args)
     {
-        $categories = Categorie::all()->where('id','=',$args['id']);
-        $res = "";
-        foreach ($categories as $category) {
-            $res .= $category->libelle . "<br>";
-            $res .= "Description : " . $category->description . "<br>";
-        }
-        if(sizeof($categories) == 0) {
+        $categories = Categorie::all();
+        $category = Categorie::where('id','=',$args['id'])->first();
+
+        if($category == null) {
             return new Response(404);
         }
-        $response->getBody()->write($res);
+
+        $html = $this->twig->render('category_by_id.html.twig', ['categories' => $categories,'category' => $category]);
+        $response->getBody()->write($html);
         return $response;
     }
 }
--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -6,11 +6,9 @@ use gift\appli\Controllers\CategoriesAction;
 use gift\appli\Controllers\PrestationAction;
 
 
-return function ($app) {
-    $app->get('/categories', new CategoriesAction());
-    $app->get('/categories/{id}', new CategorieIdAction());
 return function ($app, $twig) {
     $app->get('/categories', new CategoriesAction($twig));
+    $app->get('/categories/{id}', new CategorieIdAction($twig));
     $app->get('/prestation', new PrestationAction());
     return $app;
 };
