29b89b2dbc85af160ac5714e6b2fad19350009f8
Hugo Bena <heno.drawing@gmail.com>
2025-05-13 15:14:06 +0200

    supprime les imports inutiles

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -3,16 +3,12 @@
 
 use gift\appli\Controllers\CategorieIdAction;
 use gift\appli\Controllers\CategoriesAction;
-use gift\appli\models\Categorie;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use gift\appli\Controllers\PrestationAction;
 
 
 return function ($app) {
     $app->get('/categories', new CategoriesAction());
     $app->get('/categories/{id}', new CategorieIdAction());
-
     $app->get('/prestation', new PrestationAction());
     return $app;
 };
