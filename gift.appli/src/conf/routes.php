ea270a69860aa819e96b35436a2510d24d5e97af
Hugo Bena <heno.drawing@gmail.com>
2025-05-14 17:35:50 +0200

    rename des routes

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -5,15 +5,23 @@ use gift\appli\Controllers\CategorieIdAction;
 use gift\appli\Controllers\CategoriesAction;
 use gift\appli\Controllers\HomeAction;
 use gift\appli\Controllers\PrestationAction;
-
+use gift\appli\Controllers\PrestationParCategorieAction;
 
 return function ($app) {
-    $app->get('/categories', CategoriesAction::class);
-    $app->get('/categories/{id}', CategorieIdAction::class);
-    $app->get('/prestation', PrestationAction::class);
-    $app->get('/categories/{id}/prestations', \gift\appli\Controllers\PrestationParCategorieAction::class)
+    $app->get('/categories', CategoriesAction::class)
+        ->setName('categories.all');
+
+    $app->get('/categories/{id}', CategorieIdAction::class)
+        ->setName('categorie.parId');
+
+    $app->get('/prestation', PrestationAction::class)
+        ->setName('prestation.single');
+
+    $app->get('/categories/{id}/prestations', PrestationParCategorieAction::class)
         ->setName('prestations.parCategorie');
 
-    $app->get('/', HomeAction::class);
+    $app->get('/', HomeAction::class)
+        ->setName('Home.page');
+
     return $app;
 };
