22a9e38a4407153bfc5a740374596861fd88ae96
Riole88 <boullemanitou@gmail.com>
2025-05-14 16:44:23 +0200

    Modif try catch presentation

--- gift.appli/src/Controllers/PrestationAction.php
+++ gift.appli/src/Controllers/PrestationAction.php
@@ -2,7 +2,11 @@
 
 namespace gift\appli\Controllers;
 use gift\appli\models\Prestation;
+use Illuminate\Support\ItemNotFoundException;
 use PhpParser\Error;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Slim\Views\Twig;
@@ -10,11 +14,16 @@ use Slim\Views\Twig;
 class PrestationAction extends AbstractAction {
     public function __invoke(Request $request, Response $response, array $args)
     {
-        $twig = Twig::fromRequest($request);
-        if($request->getQueryParams() == null) {
+        try {
+            $twig = Twig::fromRequest($request);
+            $prestation = Prestation::all()->where('id', '=', $request->getQueryParams()['id'])->firstOrFail();
+        }
+        catch( HttpBadRequestException ) {
             return new Response(400);
-        } else {
-            $prestation = Prestation::all()->where('id', '=', $request->getQueryParams()['id'])->first();
+        } catch( HttpInternalServerErrorException ) {
+            return new Response(500);
+        } catch( ItemNotFoundException ) {
+            return new Response(404);
         }
         return $twig->render($response,'prestation.html.twig', ['prestation' => $prestation]);
 
