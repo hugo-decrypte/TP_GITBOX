25ff15fb163cc7badaa8cc7e9b2e6bc2d6a870c4
Riole88 <boullemanitou@gmail.com>
2025-05-14 15:28:44 +0200

    Modif twig pour prestation

--- gift.appli/src/Controllers/PrestationAction.php
+++ gift.appli/src/Controllers/PrestationAction.php
@@ -5,24 +5,18 @@ use gift\appli\models\Prestation;
 use PhpParser\Error;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use Slim\Views\Twig;
 
 class PrestationAction extends AbstractAction {
-
-    private $twig;
-    public function __construct($twig){
-        $this->twig = $twig;
-    }
-
     public function __invoke(Request $request, Response $response, array $args)
     {
+        $twig = Twig::fromRequest($request);
         if($request->getQueryParams() == null) {
             return new Response(400);
         } else {
             $prestation = Prestation::all()->where('id', '=', $request->getQueryParams()['id'])->first();
         }
-        $html = $this->twig->render('prestation.html.twig', ['prestation' => $prestation]);
-        $response->getBody()->write($html);
-        return $response;
+        return $twig->render($response,'prestation.html.twig', ['prestation' => $prestation]);
 
     }
 }
