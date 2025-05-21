f568a5a419cf74ea7f8d25ddfbc1910a709e6167
Hugo Bena <heno.drawing@gmail.com>
2025-05-21 14:57:27 +0200

    reconstruction de bootstrap et routes

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -1,41 +1,21 @@
 <?php
 
+
 use gift\appli\utils\Eloquent;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
-/**
- * Initialise la connexion à la base de données via Eloquent.
- * Le fichier 'db.ini' contient les paramètres de configuration.
- */
-Eloquent::init(__DIR__ . '/db.ini');
+Eloquent::init(__DIR__.'/db.ini');
 
-/**
- * Crée une instance de Twig pour le rendu des vues.
- * @var \Slim\Views\Twig $twig Instance de Twig avec le répertoire des vues.
- */
-$twig = Twig::create(
-    __DIR__ . '/../views',
+$twig = Twig::create(__DIR__. '/../views',
     ['cache' => 'path/to/cache-dir',
-        'auto_reload' => true]
-);
+        'auto_reload' => true]);
 
-/**
- * Crée une instance de l'application Slim.
- * @var \Slim\App $app Application Slim.
- */
+
 $app = AppFactory::create();
 
-/**
- * Ajoute le middleware Twig à l'application Slim.
- * Permet de rendre les vues Twig dans les routes.
- */
 $app->add(TwigMiddleware::create($app, $twig));
+$app = (require_once __DIR__ . '/routes.php')($app);
 
-/**
- * Inclut et exécute le fichier de définition des routes.
- *
- * @var \Slim\App $app Application mise à jour avec les routes définies.
- */
-$app = (require_once __DIR__ . '/routes.php');
+return $app;
--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -6,36 +6,24 @@ use gift\appli\Controllers\CategoriesAction;
 use gift\appli\Controllers\HomeAction;
 use gift\appli\Controllers\PrestationAction;
 use gift\appli\Controllers\PrestationParCategorieAction;
-use gift\appli\Controllers\ThemeIdAction;
 use gift\appli\Controllers\ThemesAction;
-
-
-/**
- * Déclaration des routes de l'application Slim.
- *
- * Ce fichier retourne une fonction anonyme qui reçoit l'instance de l'application
- * et y enregistre toutes les routes nécessaires.
- *
- * @param \Slim\App $app L'application Slim sur laquelle on enregistre les routes
- *
- * @return void
- */
 
 return function ($app) {
     $app->get('/categories', CategoriesAction::class)
-        ->setName('categories');
+        ->setName('categories.all');
+
     $app->get('/categories/{id}', CategorieIdAction::class)
-        ->setName('categoryById');
+        ->setName('categorie.parId');
+
     $app->get('/prestation', PrestationAction::class)
-        ->setName('prestation');
+        ->setName('prestation.single');
+
     $app->get('/categories/{id}/prestations', PrestationParCategorieAction::class)
-        ->setName('prestationsByCategory');
-    $app->get('/themes', ThemesAction::class)
-        ->setName("themes");
-    $app->get('/theme/{id}', ThemeIdAction::class);
+        ->setName('prestations.parCategorie');
+    $app->get('/theme', ThemesAction::class);
 
     $app->get('/', HomeAction::class)
-        ->setName('homepage');
+        ->setName('Home.page');
 
     return $app;
 };
