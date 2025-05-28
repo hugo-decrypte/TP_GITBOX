acbf4e83ce2805ada1c52415dcc4ec2dbdac9bbc
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-05-28 17:28:22 +0200

    gestion des erreurs orm dans le service CatalogueService

--- gift.appli/src/application_core/application/useCases/CatalogueService.php
+++ gift.appli/src/application_core/application/useCases/CatalogueService.php
@@ -2,6 +2,7 @@
 
 namespace gift\appli\application_core\application\useCases;
 
+use gift\appli\application_core\application\exceptions\DatabaseException;
 use gift\appli\application_core\domain\entities\Categorie;
 use gift\appli\application_core\domain\entities\CoffretType;
 use gift\appli\application_core\domain\entities\Prestation;
@@ -10,26 +11,51 @@ use gift\appli\application_core\domain\entities\Theme;
 class CatalogueService implements CatalogueServiceInterface {
 
     public function getCategories(): array {
-        return Categorie::all()->toArray();
+        try {
+            return Categorie::all()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération de la catégorie.");
+        }
     }
 
     public function getCategorieById(int $id): array {
-        return Categorie::where('id', $id)->first()->toArray();
+        try {
+            return Categorie::where('id', $id)->first()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération de la catégorie par son identifiant.");
+        }
+
     }
 
     public function getPrestationById(string $id): array {
-        return Prestation::where('id', $id)->first()->toArray();
+        try {
+            return Prestation::where('id', $id)->first()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération de la prestation par son identifiant.");
+        }
     }
 
     public function getPrestationsbyCategorie(int $categ_id): array {
-        return Prestation::where("cat_id", $categ_id)->get()->toArray();
+        try {
+            return Prestation::where("cat_id", $categ_id)->get()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération des prestations par sa catégorie.");
+        }
     }
 
     public function getThemesCoffrets(): array {
-        return Theme::with('coffrets')->get()->toArray();
+        try {
+            return Theme::with('coffrets')->get()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération des themes de coffrets.");
+        }
     }
 
     public function getCoffretById(int $id): array {
-        return CoffretType::where('id', $id)->first()->toArray();
+        try {
+            return CoffretType::where('id', $id)->first()->toArray();
+        } catch (\Throwable $e) {
+            throw new DatabaseException("Erreur lors de la récupération d'un coffret par son identidiant.");
+        }
     }
 }
