5bac66f3e10ff3133d23ccb824711122e085d402
bocchiarati <88000sofien@gmail.com>
2025-06-02 15:49:21 +0200

    Suppression de la route vers les Coffrets (ainsi que des vu et du controlleur lier)

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -15,8 +15,6 @@ return function ($app) {
         ->setName('categories');
     $app->get('/themes', ThemesAction::class)
         ->setName("themes");
-    $app->get('/coffrets', CoffretsAction::class)
-        ->setName('coffrets');
     $app->get('/prestations', Prestations::class)
         ->setName('prestations');
     $app->get('/creerCoffret', CreerCoffretAction::class)
