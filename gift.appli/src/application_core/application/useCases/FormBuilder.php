8c058a4f827d05a51431a2af516bc70242545551
bocchiarati <88000sofien@gmail.com>
2025-06-04 17:38:55 +0200

    Add sign-in and register form builders to FormBuilder

--- gift.appli/src/application_core/application/useCases/FormBuilder.php
+++ gift.appli/src/application_core/application/useCases/FormBuilder.php
@@ -9,18 +9,21 @@ class FormBuilder implements FormBuilderInterface {
         return [
             'actionRoute' => 'post_creer_box_perso',
             'submit_button' => 'Créer la Box',
+            'links' => [],
             'inputs' => [
                 [
                     'name' => 'libelle',
                     'label' => 'Libellé de la Box',
                     'type' => 'text',
-                    'placeholder' => 'Libellé de la box'
+                    'placeholder' => 'Libellé de la box',
+                    'required' => true
                 ],
                 [
                     'name' => 'description',
                     'label' => 'Description',
                     'type' => 'textarea',
-                    'placeholder' => 'Description de la box'
+                    'placeholder' => 'Description de la box',
+                    "required" => true,
                 ]
             ],
             'selects' => []
@@ -32,6 +35,73 @@ class FormBuilder implements FormBuilderInterface {
             'actionRoute' => 'post_creer_box_model',
             'submit_button' => "",
             'inputs' => [],
+            'selects' => []
+        ];
+    }
+
+    public function buildSignInForm(): array {
+        return [
+            'actionRoute' => 'post_signin',
+            'submit_button' => "Se connecter",
+            'links' => [
+                [
+                    "label" => "Pas encore de compte ? Créez en un dès maintenant !",
+                    "route" => "creer_compte"
+                ]
+            ],
+            'inputs' => [
+                [
+                    'name' => 'email',
+                    'label' => 'Email',
+                    'type' => 'text',
+                    'placeholder' => 'exemple@mail.com',
+                    'required' => true
+                ],
+                [
+                    'name' => 'Mot de passe',
+                    'label' => 'Mot de passe',
+                    'type' => 'password',
+                    'placeholder' => 'Renseigner votre mot de passe',
+                    "required" => true,
+                ]
+            ],
+            'selects' => []
+        ];
+    }
+
+    public function buildRegisterForm(): array {
+        return [
+            'actionRoute' => 'post_creer_compte',
+            'submit_button' => "S'enregistrer",
+            'links' => [
+                [
+                    "label" => "Déjà un compte ? Connectez vous dès maintenant !",
+                    "route" => "signin"
+                ]
+            ],
+            'inputs' => [
+                [
+                    'name' => 'email',
+                    'label' => 'Email',
+                    'type' => 'text',
+                    'placeholder' => 'exemple@mail.com',
+                    'required' => true
+                ],
+                [
+                    'name' => 'password',
+                    'label' => 'Mot de passe',
+                    'type' => 'password',
+                    'placeholder' => 'Renseigner votre mot de passe',
+                    "required" => true,
+                ],
+                [
+                    'name' => 'repeat_password',
+                    'label' => 'Mot de passe',
+                    'type' => 'password',
+                    'placeholder' => 'Renseigner votre mot de passe à nouveau',
+                    "required" => true,
+                ]
+            ],
             'selects' => []
         ];
     }
