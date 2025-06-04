27e281958813b62ec0c2dece309f8e4670affb99
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-06-04 10:49:39 +0200

    fusion branches avec auth

--- gift.appli/src/application_core/application/providers/interfaces/AuthnProviderInterface.php
+++ gift.appli/src/application_core/application/providers/interfaces/AuthnProviderInterface.php
@@ -1,8 +1,6 @@
 <?php
 
-namespace gift\appli\application_core\application\useCases\interfaces;
-
-use gift\appli\application_core\domain\entities\User;
+namespace gift\appli\application_core\application\providers\interfaces;
 
 interface AuthnProviderInterface {
     public function getSignedInUser(): array;
--- gift.appli/src/webui/actions/PostSigninAction.php
+++ gift.appli/src/webui/actions/PostSigninAction.php
@@ -3,7 +3,7 @@
 namespace gift\appli\webui\actions;
 
 use gift\appli\application_core\application\exceptions\DatabaseException;
-use gift\appli\application_core\application\useCases\SessionAuthnProvider;
+use gift\appli\application_core\application\providers\SessionAuthnProvider;
 use MongoDB\Driver\Exception\AuthenticationException;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
@@ -11,23 +11,20 @@ use Slim\Views\Twig;
 
 class PostSigninAction extends AbstractAction{
 
-
     public function __invoke(Request $request, Response $response, array $args)
     {
         $twig = Twig::fromRequest($request);
-
-            $data = $request->getParsedBody();
-            $email = $data['email'];
-            $password = $data['password'];
+        $data = $request->getParsedBody();
+        $email = $data['email'];
+        $password = $data['password'];
 
         try{
-
             $provider = new SessionAuthnProvider();
             $provider->signin($email, $password);
         } catch(DatabaseException $e) {
-            return $twig->render($response, 'error/index.html.twig', ["code" => 500, "message" => "Erreur interne du serveur, " . $e->getMessage() . " veuillez essayer plus tard."]);
-        } catch(AuthenticationException) {
-
+            return $twig->render($response, 'error/index.html.twig', ["code" => 500, "message" => "Erreur interne du serveur, " . $e->getMessage() . " Veuillez essayer plus tard."]);
+        } catch(AuthenticationException $e) {
+            return $twig->render($response, 'error/index.html.twig', ["code" => 500, "message" => "Erreur lors de l'authentification, " . $e->getMessage() . " Veuillez essayer plus tard."]);
         }
         return $twig->render($response, 'presentation/index.html.twig');
     }
